07d55f1eafdb685c335d3397f522a5d34610b5fe
root <a.mugabo2@alustudent.com>
2025-05-09 07:54:08 -0700

    correcting regular expression

--- regular_expressions/0-simply_match_school.rb
+++ regular_expressions/0-simply_match_school.rb
@@ -1,18 +1,2 @@
 #!/usr/bin/env ruby
-Holberton
-
-[Got]
-
-(0 chars log)
-
-[Expected]
-School
-(6 chars long)
-
-[Got]
-
-(0 chars log)
-
-[Expected]
-Schoolschool
-(12 chars long)
+puts ARGV[0].scan(/school/).join
